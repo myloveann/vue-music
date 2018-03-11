bf0d0f45a316b2633961d7cf1425a3c63791c008
myloveann <rj14108@163.com>
2018-03-11 13:18:13 +0800

    初始化项目目录，保证项目能够正常跑起来

---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="box">
+    <h1> Hello </h1>
+    <p>{{msg}}</p>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+  name: 'box',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+
+</style>
+
